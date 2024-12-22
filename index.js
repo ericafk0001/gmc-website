742ea6a15d79d6e3df354a44f58c27a22098218c
8percent <85259809+ericafk0001@users.noreply.github.com>
2024-12-22 02:39:15 +0000

    s

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    const header = document.getElementById(header)
+
     console.log("Welcome to Great Mission Church! What brings you to the console?")
 })
